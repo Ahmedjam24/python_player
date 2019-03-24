cf443a790043a3ec913f616bb00b933c652d8fb7
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-03-24 20:44:35 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,42 @@
+usr=raw_input()
+usr=int(usr)
+if(usr==1):
+	print "I"
+elif(usr==2):
+	print "II"
+elif(usr==3):
+	print "III"
+elif(usr==4):
+	print "IV"
+elif(usr==5):
+	print "V"
+elif(usr==6):
+	print "VI"
+elif(usr==7):
+	print "VII"
+elif(usr==8):
+	print "VIII"
+elif(usr==9):
+	print "IX"
+elif(usr==10):
+	print "X"
+elif(usr==11):
+	print "XI"
+elif(usr==12):
+	print "XII"
+elif(usr==13):
+	print "XIII"
+elif(usr==14):
+	print "XIV"
+elif(usr==15):
+	print "XV"
+elif(usr==16):
+	print "XVI"
+elif(usr==17):
+	print "XVII"
+elif(usr==18):
+	print "XVIII"
+elif(usr==19):
+	print "XIX"
+elif(usr==20):
+	print "XX"
